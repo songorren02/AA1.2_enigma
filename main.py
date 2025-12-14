d13995bbcf01b55a8fa0cb5bef05744c43773ec9
pauuquintana <paula.quintana@estudiant.enti.cat>
2025-12-14 15:44:26 +0100

    Función posiciones, carga de rotores y menú hechas
    Se ha completado la función del menú que quedaba pendiente.
    Se ha acabado la función donde se le pide al usuario la posición inicial de los rotores.
    Se han cargado los rotores con las posiciones indicadas por el usuario.

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import enigma_func as f
 import rotors as r
+NUM_ROTORES = 3
 
 
 #Leer rotores desde los archivos
-wiring_1, notch_1 = f.read_rotor("rotor1.txt")
-wiring_2, notch_2 = f.read_rotor("rotor2.txt")
-wiring_3, notch_3 = f.read_rotor("rotor3.txt")
-     
+wiring_1, notch_1 = r.read_rotor("rotors/rotor1.txt")
+wiring_2, notch_2 = r.read_rotor("rotors/rotor2.txt")
+wiring_3, notch_3 = r.read_rotor("rotors/rotor3.txt")
+
+#Pedimos al usuario la posición en la que quiere que empiecen los rotores
+def position():
+    print("En qué posición quieres que empiecen los rotores? ")
+    rotores = ["*"] * NUM_ROTORES
+    for i in range(NUM_ROTORES):
+        mensaje = input(f"Posición inicial del rotor {i+1}: ")
+        if mensaje in f.ALPH:
+            rotores[i] = mensaje
+        else:
+            print("[ERROR] Letra no aceptada, se utilizará A por defecto")
+            rotores[i] = "A"
+    
+    return rotores
+
+posiciones = position()
+
+#Cargamos cada rotor individualmente
+rotor_1 = r.rotor_load(wiring_1, notch_1, posiciones[0])
+rotor_2 = r.rotor_load(wiring_2, notch_2, posiciones[1])
+rotor_3 = r.rotor_load(wiring_3, notch_3, posiciones[2])
+        
 
 #Creamos el menú
 def print_menu():
@@ -17,17 +39,21 @@ def print_menu():
     print("3. Editar rotores")
     print("4. Salir")
 
-
+ 
 print_menu()
 
 def choose_menu():
     opt = int(input("¿Qué opción quieres hacer? "))
-    print(opt)
 
     if opt == 1:
-        f.encrypt()
+        texto = input("Introduce el texto a cifrar: ")
+        texto = f.normalizar_texto(texto)
+        print(f.encrypt(texto, rotor_1, rotor_2, rotor_3))
     elif opt == 2:
-        f.decrypt()
+        texto = input("Introduce el texto a cifrar: ")
+        texto = f.normalizar_texto(texto)
+        print(f.decrypt(texto, rotor_1, rotor_2, rotor_3))
+
     elif opt == 3:
         #Funcion editar rotor
         print("Editar rotor")
